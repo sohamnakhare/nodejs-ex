110b7a4fd2db8b65c33a16923e30fed7fd0c0b1e
soham nakhare <sohamnakhare@gmail.com>
2016-11-15 18:21:13 +0530

    added animation draft 1

--- public/javascript/homepage.js
+++ public/javascript/homepage.js
@@ -9,12 +9,37 @@
             var scrollTop = $(document).scrollTop();
 
             if (scrollTop > 0) {
-                bar.addClass('navbar-fixed-top on-scroll');
+                bar.addClass('navbar-fixed-top on-scroll animated slideInDown');
             }
 
             if (scrollTop === 0) {
-                bar.removeClass('navbar-fixed-top on-scroll','0.1s');
+                bar.removeClass('navbar-fixed-top on-scroll animated slideInDown', '0.1s');
             }
         });
+    })
+)
+
+$(document).ready(
+    $(function () {
+        $('#outcome').bind('inview', function (event, visible) {
+            animateIcons(event, visible, $('#outcome'));
+        });
+
+        $('#insights').bind('inview', function (event, visible) {
+            animateIcons(event, visible, $('#insights'));
+        });
+
+        $('#expertise').bind('inview', function (event, visible) {
+            animateIcons(event, visible, $('#expertise'));
+        });
+
+        function animateIcons(event, visible, element) {
+            if (visible === true) {
+                setTimeout(function () {
+                    element.addClass('animated fadeInUp');
+                    element.unbind('inview');
+                },300)
+            } 
+        }
     })
 )
