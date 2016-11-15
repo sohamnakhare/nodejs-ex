68672465dd41de41e3d32db7434ec57e70d151d8
soham nakhare <sohamnakhare@gmail.com>
2016-11-15 14:06:03 +0530

    reverted changes in server.js

--- server.js
+++ server.js
@@ -10,11 +10,9 @@ Object.assign=require('object-assign')
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'));
 
-var publicPath = __dirname + '/public/';
-//app.use(express.static(publicPath));
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
