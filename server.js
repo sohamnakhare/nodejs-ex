199a209de49a191b5d11995099a1f0845aadd53d
soham nakhare <sohamnakhare@gmail.com>
2016-11-15 14:14:37 +0530

    added public folder

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ Object.assign=require('object-assign')
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'));
 
+var publicPath = __dirname + '/public/';
+app.use(express.static(publicPath));
+
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
